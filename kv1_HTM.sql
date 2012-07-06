61125d74bc6934fea3e95636e3594fde74504c9f
Skywave <thakoch@gmail.com>
2012-07-06 19:57:31 +0200

    Workaround for too long HTM/9292 destnamemain

--- kv1_HTM.sql
+++ kv1_HTM.sql
@@ -3,3 +3,5 @@ alter table usrstop alter town drop not null;
 
 alter table usrstar alter name drop not null;
 alter table usrstop alter name drop not null;
+
+alter table dest alter destnamemain type VARCHAR(30);
