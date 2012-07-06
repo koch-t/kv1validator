e8a99e8d8920382f7de9dc783c9c3855c5347d65
Skywave <thakoch@gmail.com>
2012-07-06 20:07:19 +0200

    Extend the 9292/HTM workaround even further to 14 characters over the by the protocol described length

--- kv1_HTM.sql
+++ kv1_HTM.sql
@@ -4,4 +4,4 @@ alter table usrstop alter town drop not null;
 alter table usrstar alter name drop not null;
 alter table usrstop alter name drop not null;
 
-alter table dest alter destnamemain type VARCHAR(30);
+alter table dest alter destnamemain type VARCHAR(38);
