1e145b8d97c547c82844bbb4309436c8bb7b75ce
Skywave <thakoch@gmail.com>
2012-07-23 18:48:40 +0200

    Add Veolia and Qbuzz importer + Veolia GTFS exporter

--- kv1_VTN.sql
+++ kv1_VTN.sql
@@ -5,6 +5,8 @@ alter table dest drop column relevantdestnamedetail;
 alter table usrstop drop column userstoptype;
 alter table jopatili drop column productformulatype;
 alter table pujopass drop column dataownerisoperator;
+alter table line alter linevetagnumber type VARCHAR(3);
+alter table jopatili drop constraint jopatili_dataownercode_fkey;
 
 -- Some Veolia KV1 do have this column
 alter table pujopass drop column wheelchairaccessible;
